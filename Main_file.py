9025e47284cf8df3b5733930c1666ac888136084
yaohi_the_transfem <yaohiFB@proton.me>
2025-10-07 00:18:16 +0100

    Update Main_file.py
    further formatting changes + newer functions

--- Main_file.py
+++ Main_file.py
@@ -3,16 +3,18 @@
 ##for constants used in other functions set their x value to the value that nothing changes due to python's decimal number limitations
 ##pampy is an shorthand for "physics and mathematics formulae python"
 
-##discrete
-##statistics
-def arithmetic_mean(x):
-    n = len(x)
-    sum = 0
-    for ite in range(n):
-        sum += x[ite]
-    return sum/n
-        
-##combinatorics
+##basic arithmetic 
+##all of these are made for fun, delete if you want!
+def add(x, y):
+    return x+y
+def sub(x, y):
+    return -y+x
+def mul(x, y):
+    return x*y
+def div(x, y):
+    return 1/y*x
+
+##intermediate functions
 def factorial(x): ##only can take in positive integer inputs, use gamma function for negative or fractional values
     total = 1 
     if x == 0:
@@ -21,21 +23,10 @@ def factorial(x): ##only can take in positive integer inputs, use gamma function
         for ite in range(1, x):
             total *= ite 
         return total*x
+def N_factorial(x, y):
 
-def binomial_coefficient(n, k):
-    return factorial(n)/(factorial(k)*factorial(n-k))
+##store section for roots, factorials etc
 
-def binomial_theorem(x, y, n):
-    k = 0
-    total = 0
-    for ite in range(n):
-        total += (x ** ite)*(y ** (n-k))
-    return total
-
-
-
-
-##countinous
 ##constants
 def e(x):
     total = 0
@@ -50,14 +41,95 @@ def pi(x):
     total = 0
     for ite in range(x):
        total += (((-1)**ite)*((factorial(6*ite))/((factorial(3*ite))*(factorial(ite))**3))*((13591409 + 54514013 * ite)/(640320 ** (3 * ite + 1.5))))
-    return 1/12 * total ** -1
+    return 0.083333333 * total ** -1
+
+def gold_ratio():
+    return ((sqrt(5)+1)*0.5)
+
+
+##beginner functions
+
+
+
+
+
+
+
+
+
+##number theory
+def triangular_num(x):
+    return x*(x+1)*0.5
+
+
+##statistics
+def arithmetic_mean(x):
+    n = len(x)
+    sum = 0
+    for ite in range(n):
+        sum += x[ite]
+    return sum*1/n
+def binomial_coefficient(n, k):
+    return factorial(n)/(factorial(k)*factorial(n-k))
+
+def binomial_theorem(x, y, n):
+    k = 0
+    total = 0
+    for ite in range(n):
+        total += (x ** ite)*(y ** (n-k))
+    return total
+
+##euclidean geometry
+def area_square(a):
+    return a ** 2
+
+def area_rectangle(l, h):
+    return l * h
+
+def area_triangle(a):
+    return (a ** 2)/2
+
+def area_circle(r):
+    return pi(4)*(r**2)
+
+def perimetre_square(l): ##l is is the length of one of the four sides in the square
+    return l*4
+
+
+
+
+
+
+
+
+
+
+
+
+
+##Trigonometry functions
+def deg_to_rad(x):
+    return pi(4)*0.00555555555*x
+
+def rad_to_deg(x):
+    return x*180*(1/(pi(5)))
+
+def sin(x):
+
+def tan(x):
+
+def cos(x):
+    
+
+
+
+
+
+
+
 
 
 ##non beginner based composed functions
 ##lambert W function
 def W(z):
     return z*(e(64)**z)
-
-
-test = binomial_coefficient(5, 2)
-print(f"The value for the binomial coefficient function is {test}")
