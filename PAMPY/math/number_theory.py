4d6c671957d118e07f17ba67a355e754c336b9bf
_yaohi <yaohiFB@proton.me>
2025-11-27 06:48:29 +0000

    Delete PAMPY directory
    older version, can't update github repo from vscode as of now.

--- PAMPY/math/number_theory.py
+++
@@ -1,2 +0,0 @@
-def triangular_num(x):
-    return x*(x+1)*0.5
--- PAMPY/math/statistics.py
+++
@@ -1,6 +0,0 @@
-def arithmetic_mean(x):
-    n = len(x)
-    sum = 0
-    for ite in range(n):
-        sum += x[ite]
-    return sum*1/n
